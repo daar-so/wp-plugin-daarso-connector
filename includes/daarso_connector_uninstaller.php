c605864f8deefd6da6726df5a664d185236e76e2
R. Poppen <rolf@daar-om.nl>
2024-10-17 14:25:23 +0200

    De configuratie van deze plugin die in de options tabel worden bewaard een naam gegeven die overeenkomt met de plugin. Migratie toegevoegd die de gegevens van de oude plugin overneemt.

--- includes/daarso_connector_uninstaller.php
+++ includes/daarso_connector_uninstaller.php
@@ -4,8 +4,8 @@ namespace daarso\includes;
 
 class daarso_connector_uninstaller {
 	public static function uninstall(): void {
-		delete_option( daarso_connector_options::REQUEST_MESSAGE_ORIGIN_GUID );
-		delete_option( daarso_connector_options::REQUEST_MESSAGE_TARGET_GUID );
-		delete_option( daarso_connector_options::REQUEST_MESSAGE_OPENSSL_KEY );
+		delete_option( daarso_connector_options::REQUEST_CONNECTOR_MESSAGE_ORIGIN_GUID );
+		delete_option( daarso_connector_options::REQUEST_CONNECTOR_MESSAGE_TARGET_GUID );
+		delete_option( daarso_connector_options::REQUEST_CONNECTOR_MESSAGE_OPENSSL_KEY );
 	}
 }
